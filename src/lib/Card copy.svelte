87bbb10de898b6366774cb45ccd0eea4609a415d
後藤 開発 <gotoh.sys.dev@gmail.com>
2025-06-29 21:49:38 +0900

    ローカルでの作業のコミット2

--- src/lib/Card copy.svelte
+++
@@ -1,8 +0,0 @@
-<!-- ✅ カードスタイルの共通コンポーネント /src/lib/Card.svelte -->
-<script lang="ts">
-  export let maxWidth: string = 'max-w-md';
-</script>
-
-<div class={`bg-gray-800 p-8 rounded-2xl shadow-lg text-center w-full ${maxWidth} mx-auto`}>
-  <slot />
-</div>
