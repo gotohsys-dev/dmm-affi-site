39239eb54629b50af668d67e2db53072ef807fcb
後藤 開発 <gotoh.sys.dev@gmail.com>
2026-01-13 05:48:31 +0900

    widget6

--- src/lib/DmmWidget.svelte
+++ src/lib/DmmWidget.svelte
@@ -10,15 +10,27 @@
   let container;
 
   onMount(() => {
+    // コンポーネントがDOMにマウントされた後にスクリプトを動的に読み込む
     const script = document.createElement('script');
     script.src = 'https://widget-view.dmm.co.jp/js/placement.js';
     script.className = 'dmm-widget-scripts';
     script.dataset.id = dataId;
+    script.async = true;
+
+    // scriptタグをコンポーネントのコンテナに追加する
     container.appendChild(script);
+
+    // コンポーネントが破棄されるときにクリーンアップは不要
+    // SvelteがコンテナごとDOMから削除するため、追加したscriptタグも一緒に削除される
   });
 </script>
 
-<!-- DMMウィジェットのプレースホルダー -->
+<!-- ウィジェットのコンテナ -->
 <div bind:this={container}>
-  <ins class="dmm-widget-placement" data-id={dataId} style="background:transparent"></ins>
+  <!-- DMMウィジェットのプレースホルダー -->
+  <ins
+    class="dmm-widget-placement"
+    data-id={dataId}
+    style="background:transparent"
+  ></ins>
 </div>
