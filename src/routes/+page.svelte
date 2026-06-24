c5cbaf4bdb802147d6686ac03f7ad29a22e9beb2
gotohsys-dev <gotoh.sys.dev@gmail.com>
2026-06-25 05:27:05 +0900

    ウィジェット修正2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -132,8 +132,9 @@
 <div class="my-6 flex flex-wrap items-center justify-center gap-4">
 	<!-- <DmmWidget dataId="043481a98d238feacca4c97e7b47d21b" /> -->
 	<!-- <DmmBannerWidget affiliate_id="honebuto-001" banner_id="1209_300_250" /> -->
-	<DmmWidget dataId="7466dd6057bbc4243631a4b74a3580da" />
+	<!-- <DmmWidget dataId="7466dd6057bbc4243631a4b74a3580da" /> -->
 	<!-- <DmmBannerWidget affiliate_id="honebuto-001" banner_id="1526_300_250" /> -->
+	<ins class="dmm-widget-placement" data-id="7466dd6057bbc4243631a4b74a3580da" style="background:transparent"></ins><script src="https://widget-view.dmm.co.jp/js/placement.js" class="dmm-widget-scripts" data-id="7466dd6057bbc4243631a4b74a3580da"></script>
 	<ins class="widget-banner"></ins><script
 		class="widget-banner-script"
 		src="https://widget-view.dmm.co.jp/js/banner_placement.js?affiliate_id=honebuto-001&banner_id=1942_300_250"
