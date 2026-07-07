272b776dbb8b7776d21a9ed146f46ea05b9b8d59
gotohsys-dev <gotoh.sys.dev@gmail.com>
2026-07-08 06:09:47 +0900

    バナー変更9

--- src/routes/gacha-result/+page.svelte
+++ src/routes/gacha-result/+page.svelte
@@ -65,12 +65,8 @@
 </script>
 
 <div class="my-6 flex flex-wrap items-center justify-center gap-4">
-	<!-- <DmmWidget dataId="043481a98d238feacca4c97e7b47d21b" /> -->
-	<!-- <DmmBannerWidget affiliate_id="honebuto-001" banner_id="1209_300_250" /> -->
-	<!-- <DmmWidget dataId="7466dd6057bbc4243631a4b74a3580da" /> -->
-	<!-- <DmmBannerWidget affiliate_id="honebuto-001" banner_id="1526_300_250" /> -->
 	<!-- ウィジェット1 -->
-	<ins
+	<!-- <ins
 		class="dmm-widget-placement"
 		data-id="7466dd6057bbc4243631a4b74a3580da"
 		style="background:transparent"
@@ -78,17 +74,17 @@
 		src="https://widget-view.dmm.co.jp/js/placement.js"
 		class="dmm-widget-scripts"
 		data-id="7466dd6057bbc4243631a4b74a3580da"
-	></script>
+	></script> -->
 	<!-- バナー1 -->
 	<ins class="widget-banner"></ins><script
 		class="widget-banner-script"
 		src="https://widget-view.dmm.co.jp/js/banner_placement.js?affiliate_id=honebuto-001&banner_id=860_300_250"
 	></script>
 	<!-- バナー2 -->
-	<ins class="widget-banner"></ins><script
+	<!-- <ins class="widget-banner"></ins><script
 		class="widget-banner-script"
 		src="https://widget-view.dmm.co.jp/js/banner_placement.js?affiliate_id=honebuto-001&banner_id=1390_300_250"
-	></script>
+	></script> -->
 </div>
 
 {#if products.length === 0}
@@ -287,12 +283,8 @@
 {/if}
 
 <div class="my-6 flex flex-wrap items-center justify-center gap-4">
-	<!-- <DmmWidget dataId="043481a98d238feacca4c97e7b47d21b" /> -->
-	<!-- <DmmBannerWidget affiliate_id="honebuto-001" banner_id="1209_300_250" /> -->
-	<!-- <DmmWidget dataId="7466dd6057bbc4243631a4b74a3580da" /> -->
-	<!-- <DmmBannerWidget affiliate_id="honebuto-001" banner_id="1526_300_250" /> -->
 	<!-- ウィジェット1 -->
-	<ins
+	<!-- <ins
 		class="dmm-widget-placement"
 		data-id="7466dd6057bbc4243631a4b74a3580da"
 		style="background:transparent"
@@ -300,17 +292,17 @@
 		src="https://widget-view.dmm.co.jp/js/placement.js"
 		class="dmm-widget-scripts"
 		data-id="7466dd6057bbc4243631a4b74a3580da"
-	></script>
+	></script> -->
 	<!-- バナー1 -->
 	<ins class="widget-banner"></ins><script
 		class="widget-banner-script"
 		src="https://widget-view.dmm.co.jp/js/banner_placement.js?affiliate_id=honebuto-001&banner_id=860_300_250"
 	></script>
 	<!-- バナー2 -->
-	<ins class="widget-banner"></ins><script
+	<!-- <ins class="widget-banner"></ins><script
 		class="widget-banner-script"
 		src="https://widget-view.dmm.co.jp/js/banner_placement.js?affiliate_id=honebuto-001&banner_id=1390_300_250"
-	></script>
+	></script> -->
 </div>
 
 <style>
